c4a1d73f82f94d9dbf134100c5ecb45031213c4e
Stormgate1998 <jalapenojosh@gmail.com>
2023-03-21 19:20:41 -0600

    I'm sorry

---
+++ Mobile final/ViewModels/UploadFileViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Mobile_final.ViewModels
+{
+    public partial class UploadFileViewModel : ObservableObject
+    {
+        private readonly HttpClient client;
+        public UploadFileViewModel(HttpClient client)
+        {
+            this.client = client;
+        }
+        [ObservableProperty]
+        private string filePath;
+
+        [ObservableProperty]
+        private string blobkey;
+
+        [RelayCommand]
+        public async Task PickFileToUpload()
+        {
+            var result = await FilePicker.Default.PickAsync();
+            FilePath = result.FullPath;
+        }
+
+        [RelayCommand]
+        public async Task UploadFile()
+        {
+            using var form = new MultipartFormDataContent();
+            using var fileStream = new FileStream(FilePath, FileMode.Open);
+            using var fileContent = new StreamContent(fileStream);
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+            form.Add(fileContent, "file", Path.GetFileName(FilePath));
+
+            var response = await client.PutAsync($"uploadfile/{Path.GetFileName(FilePath)}", form);
+            Blobkey = await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
---
+++ MultiMediaApi/Controllers/MultiMediaController.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MediaController : Controller
+    {
+        private readonly BlobServiceClient blobclient;
+        public MediaController(BlobServiceClient blobclient)
+        {
+            this.blobclient = blobclient;
+        }
+        [HttpPut("uploadfile/{file}")]
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            var blobName = Guid.NewGuid().ToString();
+            var containerclient = blobclient.GetBlobContainerClient("videos");
+            var blobClient = containerclient.GetBlobClient(blobName);
+
+            try
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    await blobClient.UploadAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Add error handling logic here
+                throw new Exception($"Failed to upload file: {ex.Message}");
+            }
+
+            return blobName;
+        }
+    }
+}
