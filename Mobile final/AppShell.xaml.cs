b6bfcd15cb56c4de52fcf690b5bac254f0142c1a
Stormgate1998 <jalapenojosh@gmail.com>
2023-03-21 19:20:27 -0600

    Fixed everything?

--- Mobile final/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mobile_final;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Mobile final/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Mobile_final;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
