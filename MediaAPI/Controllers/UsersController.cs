ae41dd23cb118ab6f466b20ef614561223cfc64c
Breanna Nesbit <brebrenesbit02@gmail.com>
2023-03-28 18:46:33 -0600

    added user controller / started process to add to the database

---
+++ MediaAPI/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace MediaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public MultiMediaAppContext Context { get; }
+
+        public UserController(MultiMediaAppContext context)
+        {
+            Context = context;
+        }
+
+        [HttpPost]
+        public async Task PostUserAsync(User user)
+        {
+            await Context.Users.AddAsync(user);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Mobile final/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_final.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient http;
+
+        public UserService(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async void NewUserEntry(string firstname, string lastname, string username)
+        {
+            var user = new User()
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                Username = username
+            };
+            await http.PostAsJsonAsync<User>($"/api/user", user);
+        }
+
+    }
+}
