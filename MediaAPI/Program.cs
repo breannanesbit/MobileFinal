e5cf11b719dc6611d3a67bf26ab783dab53af346
Joshua Barlow <jalapenojosh@gmail.com>
2023-04-03 16:25:00 -0600

    opened Swagger to anyone

--- MediaAPI/Program.cs
+++ MediaAPI/Program.cs
@@ -26,11 +26,8 @@ BlobServiceClient blobServiceClient = new(blobstring);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseAuthorization();
 
