194575651ad6df3d9d9f19d1992caf0ad03b5720
Breanna Nesbit <brebrenesbit02@gmail.com>
2023-04-07 09:12:10 -0600

    added first and last name to display on the profile page

--- Mobile final/ViewModels/ProfileViewModel.cs
+++ Mobile final/ViewModels/ProfileViewModel.cs
@@ -17,14 +17,18 @@ namespace Mobile_final.ViewModels
         {
             this.service = service;
         }
+
         [ObservableProperty]
         private string firstName;
+        [ObservableProperty]
+        private string lastName;
 
         [RelayCommand]
         public async Task GetIt()
         {
-           var userList =  await service.GetCurrentUser();
-           FirstName = userList.FirstName;
+            var userList =  await service.GetCurrentUser();
+            FirstName = userList.FirstName;
+            LastName = userList.LastName;
         }
     }
 }
