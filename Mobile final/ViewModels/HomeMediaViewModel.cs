c8578aff624de70e38b96f116bb08f89bdea5875
Stormgate1998 <jalapenojosh@gmail.com>
2023-04-14 17:00:01 -0600

    minor changes

--- Mobile final/ViewModels/HomeMediaViewModel.cs
+++ Mobile final/ViewModels/HomeMediaViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mobile_final.Services;
+using Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mobile_final.ViewModels
 {
-    public class HomeMediaViewModel
+    public partial class HomeMediaViewModel : ObservableObject
     {
+
+        [ObservableProperty]
+        private string url;
+
+
+        [RelayCommand]
+        public void Start()
+        {
+            Url = "665e813e-7e0b-45fb-9ddd-260716909c56";
+
+        }
     }
 }
